2085f41a0e2ca9b0a18c952f99d85e3628a4a981
kashan iqbal <kashaniqbal33@gmail.com>
2025-05-10 17:10:05 +0500

    Added some feature or fixed issue

---
+++ slide_sequential_unlock_fullscreen/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Slide Sequential Unlock Fullscreen',
+    'summary': 'Progressive unlock slides based on previous completion',
+    'version': '1.0',
+    'author': 'Raghib Khesal',
+    'category': 'Website/Elearning',
+    'license': 'LGPL-3',
+    'depends': ['website_slides'],
+    'data': [
+        'views/slide_templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            # any assets you use (optional)
+        ],
+    },
+    'price': 12.55,
+    'currency': 'USD',
+    'support': 'raghibdev2001@gmail.com',
+    'installable': True,
+    'application': False,
+}
